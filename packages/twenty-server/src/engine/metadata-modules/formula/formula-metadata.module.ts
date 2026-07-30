4b0cb47d46e8c81b04b2b6dcca19fd1034f13711
Nassau-1 <enzoterrier@gmail.com>
2026-07-30 20:44:42 +0200

    feat(formula): expose create and recompute API

--- packages/twenty-server/src/engine/metadata-modules/formula/formula-metadata.module.ts
+++ packages/twenty-server/src/engine/metadata-modules/formula/formula-metadata.module.ts
@@ -4,7 +4,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { FieldMetadataEntity } from 'src/engine/metadata-modules/field-metadata/field-metadata.entity';
 import { FormulaDefinitionEntity } from 'src/engine/metadata-modules/formula/entities/formula-definition.entity';
 import { FormulaVersionEntity } from 'src/engine/metadata-modules/formula/entities/formula-version.entity';
+import { FormulaApplicationService } from 'src/engine/metadata-modules/formula/formula-application.service';
+import { FormulaController } from 'src/engine/metadata-modules/formula/formula.controller';
 import { FormulaMetadataService } from 'src/engine/metadata-modules/formula/formula-metadata.service';
+import { ObjectMetadataEntity } from 'src/engine/metadata-modules/object-metadata/object-metadata.entity';
 import { provideWorkspaceScopedRepository } from 'src/engine/twenty-orm/workspace-scoped-repository/provide-workspace-scoped-repository';
 
 @Module({
@@ -13,9 +16,12 @@ import { provideWorkspaceScopedRepository } from 'src/engine/twenty-orm/workspac
       FieldMetadataEntity,
       FormulaDefinitionEntity,
       FormulaVersionEntity,
+      ObjectMetadataEntity,
     ]),
   ],
+  controllers: [FormulaController],
   providers: [
+    FormulaApplicationService,
     FormulaMetadataService,
     provideWorkspaceScopedRepository(FormulaDefinitionEntity),
   ],
